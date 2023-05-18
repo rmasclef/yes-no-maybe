5bb1158fe0cf48d6e77eaeac2a353075b116612b
Romain Masclef <romain.masclef@gmail.com>
2023-05-18 02:24:50 +0200

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function submitForm() {
-    var form = document.getElementById("myForm");
-    var choice = form.elements["choice"].value;
-    document.getElementById("result").innerHTML = "You chose: " + choice + " ... name the place ";
-}
